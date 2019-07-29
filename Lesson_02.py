f411ea9b25fb271c16eedeb387449edfc7294137
Mihal Vitalii <migellle@gmail.com>
2019-07-29 19:49:05 +0300

    Еще больше красоты

--- Lesson_02.py
+++ Lesson_02.py
@@ -2,29 +2,26 @@
 школьнику? Сколько яблок останется в корзинке? Программа получает на вход числа `n` и `k` и должна вывести искомое
 количество яблок (два числа).'''
 
-print('Please enter an integer number: first for school boy, second for apples:')
-n = int(input())
-k = int(input())
+n = int(input('Введите количество школьников: '))
+k = int(input('Введите количество яблок: '))
 x = k // n
 y = k % n
-print('Result is: ' + str(x), str(y))
+print('Результат: ' + str(x), str(y))
 
 '''2. Дано число n. С начала суток прошло n минут. Определите, сколько часов и минут будут показывать электронные
 часы в этот момент. Программа должна вывести два числа: количество часов (от 0 до 23) и количество минут (от 0 до 59).
 Учтите, что число n может быть больше, чем количество минут в сутках.'''
 
-print('Please enter an integer number for time')
-n = int(input())
+n = int(input("Введите любое натуральное число: "))
 x = n // 60
 y = n % 60
 z = x % 24
-print('Result is: ' + str(z), str(y))
+print('Результат: ' + str(z), str(y))
 
 '''3. Напишите программу, которая приветствует пользователя, выводя слово `Hello`, введенное имя и знаки препинания
 по образцу: `Hello Kitty!`'''
 
-print('Please enter your name:')
-print("'Hello " + input() + "!'")
+print("'Hello " + input("Введите своё имя: ") + "!'")
 
 '''4. Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере
 (пробелы важны!). Первая строка содержит следующее значение, а втора строка содержит предыдущее значение введёного
@@ -40,8 +37,7 @@ print("'Hello " + input() + "!'")
         The next number for the number 0 is 1.
         The previous number for the number 0 is -1.'''
 
-print('Please enter an integer number:')
-a = int(input())
+a = int(input('Please enter an integer number:' ))
 print('The next number for the number',a, 'is', a+1)
 print('The previous number for the number',a, 'is', a-1)
 
@@ -51,10 +47,9 @@ print('The previous number for the number',a, 'is', a-1)
 чтобы их хватило на всех учеников? Программа получает на вход три натуральных числа: количество учащихся в каждом из 
 трех классов.'''
 
-print('Please enter an integer number, three times, for indicate school boys:')
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(input('Введите количество учащихся в первом классе: '))
+b = int(input('Введите количество учащихся во втором классе: '))
+c = int(input('Введите количество учащихся в третьем классе: '))
 
 s = a//2 + b//2 + c//2 + a%2 + b%2 + c%2
 
